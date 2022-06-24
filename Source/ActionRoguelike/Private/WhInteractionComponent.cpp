fc11ddb38dc820a15ea0353303ea1f84f5303fdd
Jim <40590729+Whalesea-Gustav@users.noreply.github.com>
2022-06-25 00:47:55 +0800

    Fix

--- Source/ActionRoguelike/Private/WhInteractionComponent.cpp
+++ Source/ActionRoguelike/Private/WhInteractionComponent.cpp
@@ -45,11 +45,13 @@ void UWhInteractionComponent::PrimaryInteract()
 				APawn* MyPawn = Cast<APawn>(MyOwner);
 
 				IWhGamePlayInterface::Execute_Interact(HitActor, MyPawn);
+
+				DrawDebugSphere(GetWorld(), Hit.ImpactPoint, Radius, 32, LineColor, false, 2.0f);
+
 				break;
 			}
 		}
-
-		DrawDebugSphere(GetWorld(), Hit.ImpactPoint, Radius, 32, LineColor, false, 2.0f);
+		
 	}
 
 	DrawDebugLine(GetWorld(), EyeLocation, End, LineColor, false, 2.0f, 0, 2.0f);
