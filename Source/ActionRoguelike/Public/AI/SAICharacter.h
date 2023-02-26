c7f7399827c91ca1ef559c1ef77880ac6396d219
Jim <40590729+Whalesea-Gustav@users.noreply.github.com>
2023-02-26 23:20:47 +0800

    Make AI-MinionRanged Rotation Smooth

--- Source/ActionRoguelike/Public/AI/SAICharacter.h
+++ Source/ActionRoguelike/Public/AI/SAICharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "SAICharacter.generated.h"
 
+class UPawnSensingComponent;
+
 UCLASS()
 class ACTIONROGUELIKE_API ASAICharacter : public ACharacter
 {
@@ -16,11 +18,15 @@ public:
 	ASAICharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleAnywhere, Category = "AI")
+	UPawnSensingComponent* PawnSensingComp;
+
+	UFUNCTION()
+	void OnPawnSeen(APawn* pawn);
+	
+	virtual void PostInitializeComponents() override;
+	
+	
 	
 };
